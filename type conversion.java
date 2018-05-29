e807bfcdd5a405b573b55fc56df982355e875f04
Dharanikala <dharanidharu011@gmail.com>
2018-05-29 17:13:03 +0530

    hi

---
+++ type conversion.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+ public static void main(String S[]){
+System.out.println("welcome");
+byte one =100;s
+byte two =20;
+int three = one + two;
+system.out.println(three);
+}
+}
